183b2266c3d10e2651eccbe7e3e6d28b3c36d9b3
ARdesign <andyesgrove@hotmail.com>
2026-01-07 14:01:20 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Now just import from 'three' - the import map handles the rest
+// help Now just import from 'three' - the import map handles the rest
 import * as THREE from 'three';
 import { MindARThree } from 'mindar-image-three';
 
